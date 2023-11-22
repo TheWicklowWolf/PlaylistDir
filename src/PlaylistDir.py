a36674ef72fbca02e94c2a52c5bf595ff3cc85cc
TheWicklowWolf <111055425+TheWicklowWolf@users.noreply.github.com>
2023-11-22 17:11:53 +0000

    Corrected Path
    Fixed path for Plex

--- src/PlaylistDir.py
+++ src/PlaylistDir.py
@@ -45,7 +45,7 @@ class Data_Handler:
 
     def add_playlist_to_plex(self, subfolder):
         try:
-            m3u_path = os.path.join(self.path_to_parent, self.playlists, subfolder + ".m3u")
+            m3u_path = os.path.join(self.path_to_playlists, self.playlists, subfolder + ".m3u")
             url = f"http://{self.plex_server_ip}/playlists/upload?sectionID={self.plex_library_section_id}&path={m3u_path}&X-Plex-Token={self.x_plex_token}"
             response = requests.post(url)
             if response.status_code == 200:
